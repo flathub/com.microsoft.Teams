c07572f4e26d0c64f3c8892d8e1442623271caf2
Lctrs <jerome@prmntr.me>
2020-01-02 09:57:22 +0100

    Embed desktop file
    Closes #4

--- apply_extra.sh
+++ apply_extra.sh
@@ -4,11 +4,6 @@ set -e
 
 ar p teams.deb data.tar.xz | tar -xJf -
 
-desktop_file="export/share/applications/com.microsoft.Teams.desktop"
-install -Dm0644 usr/share/applications/teams.desktop "$desktop_file"
-desktop-file-edit --set-key="Exec" --set-value="teams %U" "$desktop_file"
-desktop-file-edit --set-key="Icon" --set-value="com.microsoft.Teams" "$desktop_file"
-
 mv usr/bin .
 mkdir share
 mv usr/share/teams share/teams
