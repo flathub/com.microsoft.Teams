f7477f9e5af11c09738db47760860479ab77040c
Lctrs <Lctrs@users.noreply.github.com>
2021-04-08 13:14:02 +0000

    ar: set runtime linked library search path
    Closes #49

--- teams-build.sh
+++ teams-build.sh
@@ -6,7 +6,7 @@ install -Dm0755 apply_extra.sh ${FLATPAK_DEST}/bin/apply_extra
 install -Dm0755 teams.sh ${FLATPAK_DEST}/bin/teams
 install -Dm0644 ${FLATPAK_ID}.metainfo.xml ${FLATPAK_DEST}/share/metainfo/${FLATPAK_ID}.metainfo.xml
 
-ar p teams.deb data.tar.xz | tar -xJf -
+LD_LIBRARY_PATH="/app/lib" ar p teams.deb data.tar.xz | tar -xJf -
 
 install -Dm0644 usr/share/applications/teams.desktop ${FLATPAK_DEST}/share/applications/${FLATPAK_ID}.desktop
 sed -i s:Exec=/usr/bin/teams:Exec=teams: ${FLATPAK_DEST}/share/applications/${FLATPAK_ID}.desktop
